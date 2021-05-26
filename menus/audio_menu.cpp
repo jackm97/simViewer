79e26f809767f2f66b3e0894ed169235f2c5ddaa
Jack <jackhmyers97@gmail.com>
2021-05-25 20:31:06 -0700

    cleaned up audio menu

--- menus/audio_menu.cpp
+++ menus/audio_menu.cpp
@@ -8,8 +8,6 @@
 
 #include <string>
 
-#include <filesystem>
-
 std::string audio_filename = "";
 bool choosing_audio_file = false;
 bool is_audio_loaded = false;
@@ -21,8 +19,10 @@ AudioFile<float> audio_file;
 AudioFile<float>::AudioBuffer buffer(2);
 std::vector<float> rho_list_l;
 std::vector<float> rho_list_r;
+float head_damp = 1.f;
 
 AudioFile<float> audio_file_play;
+int n_sample = 0;
 
 float sound_amp = 1.f;
 bool do_sound = false;
@@ -97,6 +97,9 @@ void doAudioMenu()
                 ImGui::InputFloat("Sound Amplitude", &sound_amp);
         }
 
+        if (do_sound && is_audio_loaded)
+            ImGui::InputFloat("Head Damp", &head_damp);
+
 
         ImGui::Checkbox("Save Sound?", &save_sound);
 
@@ -135,7 +138,7 @@ void doAudioMenu()
                     }
                     audio_file.setAudioBuffer(buffer);
                     audio_file.setBitDepth(24);
-                    audio_file.setSampleRate(1 / lbm_solver->TimeStep() / (float)iter_per_frame);
+                    audio_file.setSampleRate(audio_file_play.getSampleRate());
                     audio_file.save(audio_save_location);
                     for (float& s : buffer[0]) {
                         s /= signal_amp;
@@ -151,8 +154,8 @@ void doAudioMenu()
             }
         }
     }
-    std::string sim_time = "Current Simulation Time(s): ";
-    sim_time += std::to_string(lbm_solver->Time());
+    std::string sim_time = "Current Audio Time(s): ";
+    sim_time += std::to_string((float)n_sample / (float) audio_file_play.getSampleRate());
     ImGui::TextUnformatted(sim_time.c_str());
     ImGui::End();
 
@@ -166,44 +169,50 @@ void updateAudio(){
 
     float Hz = 1.f/10.f;
     float w = 2 * (float) M_PI * Hz;
+    std::vector<int> i_vec, j_vec;
+    std::vector<float> rho0_vec;
     if (is_audio_loaded && do_sound && (is_animating || next_frame) && !is_calc_frame) {
-        float y = .5 * grid_length + .4 * grid_length * std::sin(w * lbm_solver->Time());
-        float x = .5 * grid_length - .4 * grid_length * std::abs(std::cos(w * lbm_solver->Time()));
-        int idx_y = y / lbm_solver->DeltaX();
-        int idx_x = x / lbm_solver->DeltaX();
-        int range = .005 * grid_size;
-        // float ux = sound_amp * ( std::sin(w * LBMSolver->Time()) );
-        float n_sample = audio_file_play.getSampleRate() * lbm_solver->Time();
         if (n_sample < audio_file_play.samples[0].size() - 1) {
-//            if (forces.size() == 0)
-//                forces.push_back(jfs::Force());
-//            forces[0].pos[0] = .5 * grid_length;
-//            forces[0].pos[1] = .5 * grid_length;
+            float t = (float)n_sample / (float)audio_file_play.getSampleRate();
+            float phi = .2;
 
-            float sample_down = audio_file_play.samples[0][(int)n_sample];
-            float sample_up = audio_file_play.samples[0][(int)n_sample + 1];
-            float sample = (n_sample - std::floor(n_sample)) * sample_up + (std::ceil(n_sample) - n_sample) * sample_down;
-            float ux = sound_amp * sample * std::abs(std::cos(w * lbm_solver->Time()));
-            float uy = -sound_amp * sample * std::sin(w * lbm_solver->Time());
-//            forces[0].force[0] = ux;
-//            lbm_solver->ForceVelocity(idx_x, idx_y, ux, uy);
-            float rho_current = lbm_solver->IndexRhoData(idx_x, idx_y);
+            float sample = audio_file_play.samples[0][n_sample];
+            n_sample += 1;
             float rho_amp = sound_amp * sample + lbm_solver->Rho0();
-            lbm_solver->AddMassSource(idx_x, idx_y, rho_amp - rho_current);
+
+            while (phi <= .8)
+            {
+                float y = .5 * grid_length + .2 * grid_length * std::sin(w * t);
+                float x = .5 * grid_length - .2 * grid_length * std::abs(std::cos(w * t));
+                int idx_y = y / lbm_solver->DeltaX();
+                int idx_x = x / lbm_solver->DeltaX();
+                if (i_vec.size() == 0 || (i_vec[i_vec.size() - 1] != idx_x || j_vec[j_vec.size() - 1] != idx_x))
+                {
+                    i_vec.push_back(idx_x);
+                    j_vec.push_back(idx_y);
+                    rho0_vec.push_back(rho_amp);
+                phi += .8;
+            }
+            lbm_solver->ForceMass(i_vec.data(), j_vec.data(), rho0_vec.data(), rho0_vec.size());
+
+            i_vec.clear();
+            j_vec.clear();
+            rho0_vec.clear();
             float head_depth = .178;
-            int min_head = (int) ((.5 * grid_length - head_depth / 2) / grid_length * (float) grid_size);
-            int max_head = (int) ((.5 * grid_length + head_depth / 2) / grid_length * (float) grid_size);
-            std::vector<int> i_vec, j_vec;
-            std::vector<float> u_zero;
+            int min_head = (int)((.5 * grid_length - head_depth / 2) / grid_length * (float)grid_size);
+            int max_head = (int)((.5 * grid_length + head_depth / 2) / grid_length * (float)grid_size);
             for (int i = min_head; i <= max_head; i++) {
-                for (int j = (int) (min_head + .1 * (max_head - min_head)); j <= .9 * max_head; j++) {
+                for (int j = min_head; j <= max_head; j++) {
                     i_vec.push_back(i);
                     j_vec.push_back(j);
-                    u_zero.push_back(0);
+                    rho0_vec.push_back(lbm_solver->Rho0());
                 }
             }
-            lbm_solver->ForceVelocity(i_vec.data(), j_vec.data(), u_zero.data(), u_zero.data(), u_zero.size());
+            lbm_solver->ForceMass(i_vec.data(), j_vec.data(), rho0_vec.data(), rho0_vec.size(), head_damp);
         }
+    }
+
+    if (is_audio_loaded && (is_animating || next_frame) && !is_calc_frame) {
     }
 
     if (is_audio_loaded && iter == (iter_per_frame - 1) && save_sound)
@@ -212,13 +221,12 @@ void updateAudio(){
     float sample_rate = 48000;
     if ( iter == iter_per_frame && !is_calc_frame && !failed_step)
     {
-        if ( is_saving && audio_file_play.getSampleRate() * lbm_solver->Time() >= current_sample_count )
+        if ( is_saving )
         {
             float head_width = .178; // m
-            int idxl = (int) ( (.5 * grid_length - head_width/2) / grid_length * (float) grid_size );
-            int idxr = (int) ( (.5 * grid_length + head_width/2) / grid_length * (float) grid_size );
-            int idx = .5 * grid_length / lbm_solver->DeltaX();
-//            float* rho_data = lbm_solver->RhoData();
+            int idxl = (int) ( (.5 * grid_length - head_width/2) / grid_length * (float) grid_size ) - 1;
+            int idxr = (int) ( (.5 * grid_length + head_width/2) / grid_length * (float) grid_size ) + 1;
+            int x_head = .5 * grid_length / lbm_solver->DeltaX();
             float mean_rho_l;
             float mean_rho_r;
             int moving_mean_samples = 10;
@@ -233,25 +241,14 @@ void updateAudio(){
                     mean_rho_r += rho_list_r[rho_list_r.size() - i]/moving_mean_samples;
                 }
             }
-//            for (int i = 0; i < grid_size*grid_size; i++)
-//                mean_rho += rho_data[i];
-//            mean_rho /= (grid_size*grid_size);
-            float rho = lbm_solver->IndexRhoData(idx, idxl);
+            float rho = lbm_solver->IndexRhoData(x_head, idxl);
             rho_list_l.push_back(rho);
             float sample = rho - mean_rho_l;
             buffer[0].push_back(sample);
-            rho = lbm_solver->IndexRhoData(idx, idxr);
+            rho = lbm_solver->IndexRhoData(x_head, idxr);
             rho_list_r.push_back(rho);
             sample = rho - mean_rho_r;
             buffer[1].push_back(sample);
-
-            // while (sample_rate * LBMSolver->Time() > current_sample_count)
-            // {
-
-            //     current_sample_count += 1;
-            // }
-
-            current_sample_count = audio_file_play.getSampleRate() * lbm_solver->Time() + 1;
         }
 
         is_saving = false;
@@ -263,6 +260,6 @@ void updateAudio(){
         buffer[1].clear();
         rho_list_l.clear();
         rho_list_r.clear();
-        current_sample_count = 0;
+        n_sample = 0;
     }
 }
